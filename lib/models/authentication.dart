b2463ee1ab0a06d8db70e2bc973dcea02fa47a64
JacobAldo <aldochd13@gmail.com>
2020-09-29 22:17:47 -0500

    first commit

--- lib/models/authentication.dart
+++ lib/models/authentication.dart
@@ -6,7 +6,7 @@ import 'http_exception.dart';
 class Authentication with ChangeNotifier {
   Future<void> Register(String email, String password) async {
     const url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBlSloi-WG_IE9B2e_oXwVsjWkffSwuHyY";
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCB8Zgbvo6XXfvfv1atsCdGLbNMWO0c8H0";
     try {
       final response = await http.post(url,
           body: json.encode({
@@ -22,23 +22,5 @@ class Authentication with ChangeNotifier {
     } catch (error) {
       throw error;
     }
-  }
-
-  Future<void> Ingresar(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBlSloi-WG_IE9B2e_oXwVsjWkffSwuHyY';
-    try {
-      final response = await http.post(url,
-          body: json.encode({
-            "email": email,
-            "password": password,
-            "returnSecureToken": true,
-          }));
-      final responseData = json.decode(response.body);
-      //print(responseData);
-      if (responseData['error'] != null) {
-        throw HttpException(responseData['error']['message']);
-      }
-    } catch (error) {}
   }
 }
